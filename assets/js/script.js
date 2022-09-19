21fb83271e9fb1dc434b5de510b451647da8d643
AlenaGM <alena.guillaume4@gmail.com>
2022-09-19 16:17:22 +0200

    upd comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,127 @@
 let date;
 let timer;
 
-document.querySelector("#cat-bus").addEventListener("click", function startMoving() {
+document
+  .querySelector("#cat-bus")
+  .addEventListener("click", function startMoving() {
+    date = new Date(); //time of click on the image => travel begins
+    let busImg = document.getElementById("cat-bus");
+    busImg.src = "assets/img/totoro-cat-bus-moving.jpg"; //img changes from static to moving
 
-    date = new Date(); //время клика на картинку = начало движения
-    let busImg = document.getElementById('cat-bus');
-    busImg.src ="assets/img/totoro-cat-bus-moving.jpg";//Заменяем статичную картинку на двужущуюся
+    document.getElementById("travel-duration").innerHTML = "";
+    document.getElementById("travel-info").innerHTML = "";
 
-    document.getElementById('travel-duration').innerHTML = '';
-    document.getElementById('travel-info').innerHTML = '';
+    if (document.getElementById("cat-bus").classList.contains("active")) {
+      //if the cat is already running = it's a second click => we stop the cat
 
-    if(document.getElementById('cat-bus').classList.contains("active")) {//если кот уже бежит => это повторный клик -> останавливаем
-
-        stopMoving();//Останавливаем кота, убираем класс "active"
-        document.getElementById('cat-bus').classList.remove("active");
-
-
+      stopMoving(); //we stop the cat, we remove class "active"
+      document.getElementById("cat-bus").classList.remove("active");
     } else {
-        document.getElementById('cat-bus').classList.add("active");
-        //на будущее, чтобы знать: есть класс "active" -> кот бежит, нет -> стоит
+      document.getElementById("cat-bus").classList.add("active");
+      //for future use, if "active" -> the cat is running, if "no" -> the cat is not running
     }
-});
+  });
 
-document.querySelector("#clickDate").addEventListener("click", function formatDate(){
+document
+  .querySelector("#clickDate")
+  .addEventListener("click", function formatDate() {
+    if (document.getElementById("cat-bus").classList.contains("active")) {
+      //we exclude possibility to start timer, if the cat is not running
 
-    if (document.getElementById('cat-bus').classList.contains("active")){//исключаем возможность запустить таймер, если кот не бежит
+      let timePassed = new Date() - date; //time from travel start until now, in msec
 
-        let timePassed = (new Date() - date);//время, которое прошло с момента начала движения в мс
+      document.getElementById("travel-info").innerHTML = "";
 
-        document.getElementById('travel-info').innerHTML = '';
+      if (timePassed / 1000 < 1) {
+        //time from travel-start until now is converted in seconds and compared to 1 sec
+        //if it is less than 1 sec:
+        document.getElementById("travel-duration").innerHTML =
+          "Вы выехали: прямо сейчас";
+      } else if (timePassed / 1000 < 59) {
+        //time from travel-start until now is converted in seconds and compared to 1 minute
+        //if it is less than 1 minute:
+        document.getElementById(
+          "travel-duration"
+        ).innerHTML = `Вы выехали: ${Math.round(timePassed / 1000)} сек. назад`;
+      } else if (timePassed / 1000 < 3599) {
+        //time from travel-start until now is converted in seconds and compared to 1 hour
+        //if it is less than 1 hour:
+        document.getElementById(
+          "travel-duration"
+        ).innerHTML = `Вы выехали: ${Math.round(
+          timePassed / 60000
+        )} мин. назад`;
+      } else {
+        //if it is more than 1 hour, we show full date in `DD.MM.YY HH:mm` format.
+        document.getElementById(
+          "travel-duration"
+        ).innerHTML = `Вы выехали: ${new Intl.DateTimeFormat("ru-RU", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "2-digit",
+        }).format(date)}
+            в ${new Intl.DateTimeFormat("ru-RU", {
+              hour: "2-digit",
+              minute: "2-digit",
+            }).format(date)}`;
+      }
 
-        if (timePassed/1000 < 1) {//переводим время, прошедшее с момента начала движения в секунды и сравниваем с 1 сек.
-            //если прошло меньше 1 сек.
-            document.getElementById('travel-duration').innerHTML = 'Вы выехали: прямо сейчас';
-
-        } else if (timePassed/1000 < 59){//переводим время, прошедшее с момента начала движения в секунды и сравниваем с 1 мин.
-            //если прошло меньше 1 мин.
-            document.getElementById('travel-duration').innerHTML = `Вы выехали: ${Math.round(timePassed/1000)} сек. назад`;
-
-        } else if (timePassed/1000 < 3599){//переводим время, прошедшее с момента начала движения в секунды и сравниваем с 1 часом
-            //если прошло меньше 1 часа.
-            document.getElementById('travel-duration').innerHTML = `Вы выехали: ${Math.round(timePassed/60000)} мин. назад`;
-
-        } else {//если прошло больше часа, показываем дату начала движения в формате ДД.ММ.ГГ, ЧЧ:ММ
-            document.getElementById('travel-duration').innerHTML =
-            `Вы выехали: ${new Intl.DateTimeFormat('ru-RU', {day:'2-digit', month:'2-digit', year:'2-digit'}).format(date)}
-            в ${new Intl.DateTimeFormat('ru-RU', {hour:'2-digit', minute:'2-digit'}).format(date)}`;
-        }
-
-        timer = setTimeout(formatDate,1000);//Чтобы таймер крутился без остановки
-
-    } else {//случай, когда кот еще/уже не бежит: таймер не крутится, время в пути отсутствует
-        document.getElementById('travel-duration').innerHTML = `Не знаю: сейчас вы не путешествуете.<br> Может, поедем уже?`;
-        document.getElementById('travel-info').innerHTML = '';
+      timer = setTimeout(formatDate, 1000); //we make timer turn nonstop
+    } else {
+      //case when cat is not running or stopped running => timer is not turning and there is no travel time to show
+      document.getElementById(
+        "travel-duration"
+      ).innerHTML = `Не знаю: сейчас вы не путешествуете.<br> Может, поедем уже?`;
+      document.getElementById("travel-info").innerHTML = "";
     }
+  });
+
+document.querySelector("#arrDest").addEventListener("click", function () {
+  //we press the button and stop the cat
+  stopMoving();
 });
 
-document.querySelector("#arrDest").addEventListener("click", function(){//Остановка при клике на кнопку
-    stopMoving();
-});
-
-document.querySelector("#resetTimer").addEventListener('click', function(){
-    location.reload();
+document.querySelector("#resetTimer").addEventListener("click", function () {
+  location.reload();
 });
 
 function stopMoving() {
+  if (document.getElementById("cat-bus").classList.contains("active")) {
+    //Results reporting (only if the cat had been running and arrived)
 
-    if (document.getElementById('cat-bus').classList.contains("active")){//Докладываем о результатах, только если кот побегал и прибежал
+    let busImg = document.getElementById("cat-bus");
+    busImg.src = "assets/img/totoro-cat-bus.jpg"; //img changes from moving to static
 
-        let busImg = document.getElementById('cat-bus');
-        busImg.src ="assets/img/totoro-cat-bus.jpg";//Заменяем движущееся изображение на статичное
+    document.getElementById("travel-duration").innerHTML = ""; //Showing result
+    document.getElementById(
+      "travel-info"
+    ).innerHTML = `Путешествие началось ${new Intl.DateTimeFormat("ru-RU", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+    }).format(date)}
+        в ${new Intl.DateTimeFormat("ru-RU", {
+          hour: "2-digit",
+          minute: "2-digit",
+        }).format(date)}; <br>
+        Путешествие закончилось ${new Intl.DateTimeFormat("ru-RU", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "2-digit",
+        }).format(new Date())}
+        в ${new Intl.DateTimeFormat("ru-RU", {
+          hour: "2-digit",
+          minute: "2-digit",
+        }).format(new Date())};`;
 
-        document.getElementById('travel-duration').innerHTML = '';//Выводим результаты
-        document.getElementById('travel-info').innerHTML =
-        `Путешествие началось ${new Intl.DateTimeFormat('ru-RU', {day:'2-digit', month:'2-digit', year:'2-digit'}).format(date)}
-        в ${new Intl.DateTimeFormat('ru-RU', {hour:'2-digit', minute:'2-digit'}).format(date)}; <br>
-        Путешествие закончилось ${new Intl.DateTimeFormat('ru-RU', {day:'2-digit', month:'2-digit', year:'2-digit'}).format(new Date)}
-        в ${new Intl.DateTimeFormat('ru-RU', {hour:'2-digit', minute:'2-digit'}).format(new Date)};`
+    document.getElementById("cat-bus").classList.remove("active");
 
-        document.getElementById('cat-bus').classList.remove("active");
-
-        clearTimeout(timer);//Останавливаем таймер
-
-
-    } else {//случай, когда кот не бегал = результатов нет
-        document.getElementById('travel-duration').innerHTML = ``;
-        document.getElementById('travel-info').innerHTML = `Вы никуда не приехали,<br> потому что ниоткуда не выезжали`;
-    }
-
-};
+    clearTimeout(timer); //We stop the timer
+  } else {
+    //case when the cat has not run = no results to show
+    document.getElementById("travel-duration").innerHTML = ``;
+    document.getElementById(
+      "travel-info"
+    ).innerHTML = `Вы никуда не приехали,<br> потому что ниоткуда не выезжали`;
+  }
+}
